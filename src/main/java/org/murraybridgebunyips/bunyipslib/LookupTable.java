7ebc487aff0b32311accae37f962ab1291b305ea
Lucas Bubner <lkbubner@proton.me>
2024-03-20 23:19:43 +1030

    Task improvements

--- src/main/java/org/murraybridgebunyips/bunyipslib/LookupTable.java
+++ src/main/java/org/murraybridgebunyips/bunyipslib/LookupTable.java
@@ -29,8 +29,8 @@ public class LookupTable<T extends Number, R> extends TreeMap<T, R> {
      * @return the closest value to the input key
      */
     public R getClosest(T key) {
-        Map.Entry<T, R> ceil = ceilingEntry(key);
-        Map.Entry<T, R> floor = floorEntry(key);
+        Entry<T, R> ceil = ceilingEntry(key);
+        Entry<T, R> floor = floorEntry(key);
 
         if (ceil != null && floor != null) {
             double keyVal = key.doubleValue();
