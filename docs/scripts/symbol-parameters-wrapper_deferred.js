d8da2075a28e9ebfba879c03b751003291fe0d68
Lucas Bubner <lkbubner@proton.me>
2024-08-05 12:27:52 +0930

    Update docs and Gradle configurations

--- docs/scripts/symbol-parameters-wrapper_deferred.js
+++ docs/scripts/symbol-parameters-wrapper_deferred.js
@@ -59,6 +59,6 @@
     if (document.readyState === 'loading') window.addEventListener('DOMContentLoaded', initHandlers);
     else initHandlers();
 
-    // Add `unobserve` if dokka will be SPA-like:
+    // ToDo: Add `unobserve` if dokka will be SPA-like:
     //       https://github.com/w3c/csswg-drafts/issues/5155
 })();
