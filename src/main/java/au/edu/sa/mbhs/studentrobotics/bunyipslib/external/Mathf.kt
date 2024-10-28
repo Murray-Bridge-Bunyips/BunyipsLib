434cbd230ea3dc432c3a9a712fa8c388d065f872
Lucas Bubner <bubner@proton.me>
2024-10-29 00:46:10 +1030

    Revert to valid negative maxDelta

--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/external/Mathf.kt
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/external/Mathf.kt
@@ -391,7 +391,7 @@ object Mathf {
      * Moves a current value towards `target`.
      *
      * @param target   The value to move towards.
-     * @param maxDelta The (positive) maximum change that should be applied to the value.
+     * @param maxDelta The maximum change that should be applied to the value. Negative values will push the current value away from the target for potential convergence of wrapped numbers.
      * @return The new value.
      */
     @JvmStatic
@@ -399,8 +399,6 @@ object Mathf {
         val currentD = this.toDouble()
         val targetD = target.toDouble()
         val maxDeltaD = maxDelta.toDouble()
-        // Check direction of delta, if it's invalid then return the current value
-        if (maxDeltaD < 0) return currentD
         if (FastMath.abs(targetD - currentD) <= maxDeltaD) return targetD
         return currentD + FastMath.signum(targetD - currentD) * maxDeltaD
     }
