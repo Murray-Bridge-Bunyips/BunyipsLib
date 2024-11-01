cf65a21fb8ade5dd92c508309c06a1cd9143af27
Lucas Bubner <lkbubner@proton.me>
2024-11-01 10:55:02 +1030

    Start reimplementing AprilTag pose estimation

---
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/localization/accumulators/AprilTagRelocalizingAccumulator.java
@@ -0,0 +1,179 @@
+package au.edu.sa.mbhs.studentrobotics.bunyipslib.localization.accumulators;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+import com.acmerobotics.roadrunner.Vector2d;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.Dbg;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Dashboard;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Filter;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Geometry;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.vision.data.AprilTagData;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.vision.processors.AprilTag;
+
+/**
+ * Combines an {@link AprilTag} processor with an {@link Accumulator} to relocalize the robot.
+ * This serves as the modernised version of the previous {@code AprilTagPoseEstimator}.
+ *
+ * @author Lucas Bubner, 2024
+ * @since 6.0.0
+ */
+public class AprilTagRelocalizingAccumulator extends Accumulator {
+    /**
+     * Default R gain for Kalman filtering.
+     */
+    public static double DEFAULT_R = 4;
+    /**
+     * Default Q gain for Kalman filtering.
+     */
+    public static double DEFAULT_Q = 1.0e-4;
+
+    private final AprilTag processor;
+    private final HashSet<Predicate<AprilTagData>> filters = new HashSet<>();
+    private final ArrayList<Pose2d> estimates = new ArrayList<>();
+
+    private Filter.Kalman xf;
+    private Filter.Kalman yf;
+    private Filter.Kalman rf;
+
+    private boolean active = true;
+    private boolean updateHeading = true;
+
+    /**
+     * Create a new AprilTag relocalizing accumulator.
+     *
+     * @param processor the processor to use for relocalization, assumes that the camera pose property on the builder
+     *                  has been set (see {@link AprilTag#setCameraPose(AprilTagProcessor.Builder)} for a util)
+     */
+    public AprilTagRelocalizingAccumulator(AprilTag processor) {
+        this.processor = processor;
+        setKalmanGains(DEFAULT_R, DEFAULT_Q);
+        Dashboard.enableConfig(getClass());
+    }
+
+    /**
+     * Set the gains used in the Kalman filter for the x, y, and r components.
+     *
+     * @param R higher values of R will put more trust in the odometry
+     * @param Q higher values of Q trusts the AprilTag more
+     * @return this
+     * @since 4.1.0
+     */
+    public AprilTagRelocalizingAccumulator setKalmanGains(double R, double Q) {
+        xf = new Filter.Kalman(R, Q);
+        yf = new Filter.Kalman(R, Q);
+        rf = new Filter.Kalman(R, Q);
+        return this;
+    }
+
+    /**
+     * Add a data filter that will apply to the detections of the processor whether to accept processing this tag.
+     *
+     * @param filter the filter to add
+     * @return this
+     * @since 5.1.0
+     */
+    public AprilTagRelocalizingAccumulator addDataFilter(Predicate<AprilTagData> filter) {
+        filters.add(filter);
+        return this;
+    }
+
+    /**
+     * Remove a filter instance added in {@link #addDataFilter(Predicate)}
+     *
+     * @param filter the filter to remove
+     * @return this
+     * @since 5.1.0
+     */
+    public AprilTagRelocalizingAccumulator removeDataFilter(Predicate<AprilTagData> filter) {
+        if (!filters.remove(filter))
+            Dbg.warn(getClass(), "Unable to remove filter '%', not found.", filter);
+        return this;
+    }
+
+    /**
+     * Whether to set the drive pose to the vision estimate. Default is on.
+     *
+     * @param setPoseAutomatically whether AprilTagRelocalizingAccumulator is on
+     * @return this
+     */
+    public AprilTagRelocalizingAccumulator setActive(boolean setPoseAutomatically) {
+        active = setPoseAutomatically;
+        return this;
+    }
+
+    /**
+     * Whether to set the drive pose to the vision estimate with heading information. Default is ON.
+     *
+     * @param setHeadingAutomatically whether to also set the heading to the AprilTag estimate or not
+     * @return this
+     */
+    public AprilTagRelocalizingAccumulator setHeadingEstimate(boolean setHeadingAutomatically) {
+        updateHeading = setHeadingAutomatically;
+        return this;
+    }
+
+    /**
+     * Run a standard accumulation while also relocalizing based on the presence of library AprilTags.
+     *
+     * @param twist the change in position and velocity with respect to time
+     */
+    public void accumulate(@NonNull Twist2dDual<Time> twist) {
+        super.accumulate(twist);
+        if (!active)
+            return;
+
+        // Future: latency compensation techniques
+
+        ArrayList<AprilTagData> data = processor.getData();
+        if (data.isEmpty())
+            return;
+
+        estimates.clear();
+        for (int i = 0; i < data.size(); i++) {
+            AprilTagData aprilTag = data.get(i);
+            Optional<Pose3D> robotPose = aprilTag.getRobotPose();
+            if (!robotPose.isPresent() || filters.stream().anyMatch(f -> !f.test(aprilTag))) {
+                // No luck with this ID
+                continue;
+            }
+
+            Pose3D pose = robotPose.get();
+
+            // Convert to Robot Coordinate System
+            // noinspection SuspiciousNameCombination
+            double x = pose.getPosition().y;
+            double y = -pose.getPosition().x;
+            double r = -pose.getOrientation().getYaw(AngleUnit.RADIANS);
+
+            estimates.add(new Pose2d(x, y, r));
+        }
+
+        Vector2d positionAvg = estimates.stream()
+                .map(p -> p.position)
+                .reduce(Geometry.zeroVec(), Vector2d::plus)
+                .div(estimates.size());
+        Rotation2d headingAvg = Rotation2d.exp(
+                estimates.stream()
+                        .map(p -> p.heading.toDouble())
+                        .reduce(0.0, Double::sum) / estimates.size()
+        );
+
+        // TODO: kf not implemented, determine offset for optimal kf too
+
+        pose = new Pose2d(positionAvg, updateHeading ? headingAvg : pose.heading);
+    }
+}
--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/vision/processors/AprilTag.java
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/vision/processors/AprilTag.java
@@ -1,11 +1,18 @@
 package au.edu.sa.mbhs.studentrobotics.bunyipslib.vision.processors;
 
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Degrees;
+import static au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Inches;
+
 import android.graphics.Canvas;
 import android.util.Size;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -16,6 +23,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Angle;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Distance;
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Measure;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.vision.Processor;
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.vision.data.AprilTagData;
 
@@ -48,6 +58,16 @@ public class AprilTag extends Processor<AprilTagData> {
      */
     public AprilTag(@NonNull Function<AprilTagProcessor.Builder, AprilTagProcessor.Builder> customBuilderSettings) {
         instance = customBuilderSettings.apply(makeBuilderWithCommonSettings()).build();
+    }
+
+    /**
+     * Creates a builder to assist in setting the camera pose for the AprilTagProcessor.
+     *
+     * @param builder the builder to apply the camera pose to
+     * @return the builder to set the camera pose
+     */
+    public static AprilTagCameraPoseBuilder setCameraPose(AprilTagProcessor.Builder builder) {
+        return new AprilTagCameraPoseBuilder(builder);
     }
 
     private AprilTagProcessor.Builder makeBuilderWithCommonSettings() {
@@ -110,5 +130,164 @@ public class AprilTag extends Processor<AprilTagData> {
         Size dimensions = getCameraDimensions();
         if (dimensions == null || ctx == null) return;
         instance.onDrawFrame(canvas, dimensions.getWidth(), dimensions.getHeight(), 1.0f, 1.0f, ctx);
+    }
+
+    /**
+     * Utility for setting the camera pose for the AprilTagProcessor.
+     */
+    public static class AprilTagCameraPoseBuilder {
+        private final AprilTagProcessor.Builder builder;
+        private double forwardIn;
+        private double rightIn;
+        private double upIn;
+        private double yawDeg;
+        private double pitchDeg = -90;
+        private double rollDeg;
+
+        /**
+         * Construct a new AprilTagCameraPoseBuilder
+         *
+         * @param builder the builder to apply the camera pose to
+         */
+        public AprilTagCameraPoseBuilder(AprilTagProcessor.Builder builder) {
+            this.builder = builder;
+        }
+
+        /**
+         * Set the position of the camera relative to the robot's center of rotation.
+         *
+         * @param position the raw {@link Position} instance to use, must follow the standard FTC coordinate system
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder withCameraPosition(Position position) {
+            forwardIn = position.x;
+            // noinspection SuspiciousNameCombination
+            rightIn = position.y;
+            upIn = position.z;
+            return this;
+        }
+
+        /**
+         * Set the position of the camera relative to the robot's center of rotation.
+         * Defaults to the camera being directly above the robot's center of rotation at field height.
+         *
+         * @param forward the distance forward of the robot's center of rotation (+forward is forward)
+         * @param left    the distance left of the robot's center of rotation (+left is left)
+         * @param up      the distance above the robot's center of rotation at field height (+up is up)
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder withCameraPosition(Measure<Distance> forward, Measure<Distance> left, Measure<Distance> up) {
+            forwardIn = forward.in(Inches);
+            rightIn = left.negate().in(Inches);
+            upIn = up.in(Inches);
+            return this;
+        }
+
+        /**
+         * Set the orientation of the camera relative to the robot's center of rotation.
+         *
+         * @param orientation the raw {@link YawPitchRollAngles} instance to use, must follow the standard FTC coordinate system
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder withCameraOrientation(YawPitchRollAngles orientation) {
+            yawDeg = orientation.getYaw(AngleUnit.DEGREES);
+            pitchDeg = orientation.getPitch(AngleUnit.DEGREES);
+            rollDeg = orientation.getRoll(AngleUnit.DEGREES);
+            return this;
+        }
+
+        /**
+         * Set the orientation of the camera relative to the robot's center of rotation.
+         *
+         * @param yaw   the yaw angle of the camera, zero defaults to forward, +yaw is left, -yaw is right
+         * @param pitch the pitch angle of the camera, zero defaults to parallel with the ground, +pitch is up, -pitch is down
+         * @param roll  the roll angle of the camera, zero defaults to no roll, +roll is clockwise, -roll is counterclockwise
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder withCameraOrientation(Measure<Angle> yaw, Measure<Angle> pitch, Measure<Angle> roll) {
+            yawDeg = yaw.in(Degrees);
+            pitchDeg = -90 + pitch.in(Degrees);
+            rollDeg = roll.in(Degrees);
+            return this;
+        }
+
+        /**
+         * Set the forward distance of the camera relative to the robot's center of rotation.
+         *
+         * @param forward the distance forward of the robot's center of rotation (+forward is forward)
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder forward(Measure<Distance> forward) {
+            forwardIn = forward.in(Inches);
+            return this;
+        }
+
+        /**
+         * Set the left distance of the camera relative to the robot's center of rotation.
+         *
+         * @param left the distance left of the robot's center of rotation (+left is left)
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder left(Measure<Distance> left) {
+            rightIn = left.negate().in(Inches);
+            return this;
+        }
+
+        /**
+         * Set the up distance of the camera relative to the robot's center of rotation.
+         *
+         * @param up the distance above the robot's center of rotation at field height (+up is up)
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder up(Measure<Distance> up) {
+            upIn = up.in(Inches);
+            return this;
+        }
+
+        /**
+         * Set the yaw angle of the camera relative to the robot's center of rotation.
+         *
+         * @param yaw the yaw angle of the camera, zero defaults to forward, +yaw is left, -yaw is right
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder yaw(Measure<Angle> yaw) {
+            yawDeg = yaw.in(Degrees);
+            return this;
+        }
+
+        /**
+         * Set the pitch angle of the camera relative to the robot's center of rotation.
+         *
+         * @param pitch the pitch angle of the camera, zero defaults to parallel with the ground, +pitch is up, -pitch is down
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder pitch(Measure<Angle> pitch) {
+            pitchDeg = -90 + pitch.in(Degrees);
+            return this;
+        }
+
+        /**
+         * Set the roll angle of the camera relative to the robot's center of rotation.
+         *
+         * @param roll the roll angle of the camera, zero defaults to no roll, +roll is clockwise, -roll is counterclockwise
+         * @return this instance
+         */
+        public AprilTagCameraPoseBuilder roll(Measure<Angle> roll) {
+            rollDeg = roll.in(Degrees);
+            return this;
+        }
+
+        /**
+         * Apply the configured camera pose to the builder.
+         *
+         * @return the original builder instance with the camera pose applied
+         */
+        public AprilTagProcessor.Builder apply() {
+            builder.setCameraPose(
+                    new Position(DistanceUnit.INCH, rightIn, forwardIn, upIn, 0),
+                    new YawPitchRollAngles(AngleUnit.DEGREES, yawDeg, pitchDeg, rollDeg, 0)
+            );
+            return builder;
+        }
     }
 }
