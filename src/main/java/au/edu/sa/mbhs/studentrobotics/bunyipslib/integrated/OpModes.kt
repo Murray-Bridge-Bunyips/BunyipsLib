c983b7aa9a551a37e9d447aa6f6a9b188ce572da
Lucas Bubner <lkbubner@proton.me>
2025-02-20 00:34:46 +1030

    Use a Sinister hook for OpMode registration
    Switch OpMode registration to class reference so we don't have any cross-OpMode leaks as well (this used to be a problem although the OpModes themselves were redesigned to be safe, as BOM is not instance-reusable safe)

--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/integrated/OpModes.kt
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/integrated/OpModes.kt
@@ -1,11 +1,8 @@
-package au.edu.sa.mbhs.studentrobotics.bunyipslib.integrated;
+package au.edu.sa.mbhs.studentrobotics.bunyipslib.integrated
 
-import androidx.annotation.NonNull;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
-import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
-
-import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
+import com.qualcomm.robotcore.eventloop.opmode.AnnotatedOpModeManager
+import dev.frozenmilk.sinister.apphooks.OpModeRegistrar
+import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta
 
 /**
  * Integrated BunyipsLib OpModes.
@@ -13,61 +10,55 @@ import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
  * @author Lucas Bubner, 2024
  * @since 6.0.0
  */
-public final class OpModes {
-    private static boolean suppressOpModes = false;
-
-    private OpModes() {
-        throw new AssertionError("No instances");
-    }
+object OpModes : OpModeRegistrar {
+    private var suppressOpModes = false
 
     /**
      * Call this method before start to suppress all BunyipsLib OpModes from appearing in the OpMode list.
      */
-    public static void suppressOpModes() {
-        suppressOpModes = true;
+    @JvmStatic
+    fun suppressOpModes() {
+        suppressOpModes = true
     }
-
+    
     /**
      * Register all the BunyipsLib integrated OpModes.
      *
-     * @param manager used manager
+     * @param opModeManager used manager
      */
-    @OpModeRegistrar
-    public static void registerOpModes(@NonNull OpModeManager manager) {
-        if (suppressOpModes)
-            return;
-        manager.register(
-                new OpModeMeta.Builder()
-                        .setName("Hardware Tester")
-                        .setFlavor(OpModeMeta.Flavor.TELEOP)
-                        // Using the FtcDashboard group for all of these OpModes to keep the OpMode out of the way
-                        .setGroup("dash")
-                        .build(),
-                new HardwareTester()
-        );
-        manager.register(
-                new OpModeMeta.Builder()
-                        .setName("Reset Last Known Positions")
-                        .setFlavor(OpModeMeta.Flavor.TELEOP)
-                        .setGroup("dash")
-                        .build(),
-                new ResetLastKnowns()
-        );
-        manager.register(
-                new OpModeMeta.Builder()
-                        .setName("Reset Motor Encoders")
-                        .setFlavor(OpModeMeta.Flavor.TELEOP)
-                        .setGroup("dash")
-                        .build(),
-                new ResetEncoders()
-        );
-        manager.register(
-                new OpModeMeta.Builder()
-                        .setName("Reset Robot Controller Lights")
-                        .setFlavor(OpModeMeta.Flavor.TELEOP)
-                        .setGroup("dash")
-                        .build(),
-                new ResetRobotControllerLights()
-        );
+    override fun registerOpModes(opModeManager: AnnotatedOpModeManager) {
+        if (suppressOpModes) return
+        opModeManager.register(
+            OpModeMeta.Builder()
+                .setName("Hardware Tester")
+                .setFlavor(OpModeMeta.Flavor.TELEOP) // Using the FtcDashboard group for all of these OpModes to keep the OpMode out of the way
+                .setGroup("dash")
+                .build(),
+            HardwareTester::class.java
+        )
+        opModeManager.register(
+            OpModeMeta.Builder()
+                .setName("Reset Last Known Positions")
+                .setFlavor(OpModeMeta.Flavor.TELEOP)
+                .setGroup("dash")
+                .build(),
+            ResetLastKnowns::class.java
+        )
+        opModeManager.register(
+            OpModeMeta.Builder()
+                .setName("Reset Motor Encoders")
+                .setFlavor(OpModeMeta.Flavor.TELEOP)
+                .setGroup("dash")
+                .build(),
+            ResetEncoders::class.java
+        )
+        opModeManager.register(
+            OpModeMeta.Builder()
+                .setName("Reset Robot Controller Lights")
+                .setFlavor(OpModeMeta.Flavor.TELEOP)
+                .setGroup("dash")
+                .build(),
+            ResetRobotControllerLights::class.java
+        )
     }
 }
