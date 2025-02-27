5ee1574517d95acd93ac1d3ad0dc01ba71daf06e
Lucas Bubner <lkbubner@proton.me>
2025-02-27 19:20:22 +1030

    Report motor name on `ResetEncoders`

--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/integrated/ResetEncoders.java
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/integrated/ResetEncoders.java
@@ -1,9 +1,14 @@
 package au.edu.sa.mbhs.studentrobotics.bunyipslib.integrated;
 
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.EmergencyStop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.Dbg;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 /**
  * Reset OpMode to clear all motors of their last known positions.
@@ -13,11 +18,23 @@ import au.edu.sa.mbhs.studentrobotics.bunyipslib.Dbg;
  */
 public final class ResetEncoders extends LinearOpMode {
     @Override
+    @SuppressWarnings("unchecked")
     public void runOpMode() {
-        for (DcMotor motor : hardwareMap.getAll(DcMotor.class)) {
-            Dbg.log(getClass(), "Resetting encoder on % from % ticks ...", motor.getConnectionInfo(), motor.getCurrentPosition());
-            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Map<String, DcMotor> map;
+        try {
+            Field mapping = HardwareMap.DeviceMapping.class.getDeclaredField("map");
+            mapping.setAccessible(true);
+            map = (Map<String, DcMotor>) mapping.get(hardwareMap.dcMotor);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new EmergencyStop("Failed to access HardwareMap fields!");
+        }
+        if (map != null) {
+            for (Map.Entry<String, DcMotor> motor : map.entrySet()) {
+                DcMotor m = motor.getValue();
+                Dbg.log(getClass(), "Resetting encoder on %(%) from % ticks ...", motor.getKey(), m.getConnectionInfo(), m.getCurrentPosition());
+                m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
         }
         terminateOpModeNow();
     }
