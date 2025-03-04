a5796c7b908651195393666828a76938719c3ddb
Lachlan Paul <lachlanpaul03@gmail.com>
2025-03-04 16:59:24 +1030

    very weird code

---
+++ MeepMeep/src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/roadrunner/meepmeep/shims/internal/transforms/MirroredPoseMap.kt
@@ -0,0 +1,21 @@
+package au.edu.sa.mbhs.studentrobotics.bunyipslib.roadrunner.meepmeep.shims.internal.transforms
+
+import com.acmerobotics.roadrunner.Arclength
+import com.acmerobotics.roadrunner.Pose2dDual
+import com.acmerobotics.roadrunner.PoseMap
+
+/**
+ * Perform a reflection across the X-axis.
+ * This reflects Red or Blue trajectories across the alliance plane, such as in the CENTERSTAGE field.
+ *
+ * (x,y,r) -> (x,-y,-r)
+ * @author Lucas Bubner, 2024
+ * @since 6.0.0
+ */
+open class MirroredPoseMap : PoseMap {
+    /**
+     * Applies (x,y,r) -> (x,-y,-r)
+     */
+    override fun map(pose: Pose2dDual<Arclength>) =
+        Pose2dDual(pose.position.x, -pose.position.y, pose.heading.inverse())
+}
