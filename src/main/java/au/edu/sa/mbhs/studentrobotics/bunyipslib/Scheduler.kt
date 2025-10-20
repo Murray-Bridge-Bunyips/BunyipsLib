91764fe65627b67b37eed59aaa081eccb798164b
Lucas Bubner <lkbubner@proton.me>
2025-10-20 13:23:07 +1030

    Implement more of new Scheduler and use self-typing Condition

--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/Scheduler.kt
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/Scheduler.kt
@@ -1,10 +1,17 @@
 package au.edu.sa.mbhs.studentrobotics.bunyipslib
 
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.Mathf.round
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Units.Seconds
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.logic.Condition
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.IdleTask
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.bases.Task
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Dbg
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Exceptions
 import au.edu.sa.mbhs.studentrobotics.bunyipslib.util.Text
+import au.edu.sa.mbhs.studentrobotics.bunyipslib.tasks.bases.RepeatTask
+import com.qualcomm.robotcore.hardware.Gamepad
 import dev.frozenmilk.util.cell.LateInitCell
-import java.util.function.BiFunction
+import java.util.function.BiConsumer
 import java.util.function.BooleanSupplier
 
 /**
@@ -17,11 +24,15 @@ import java.util.function.BooleanSupplier
  */
 object Scheduler {
     private val subsystemsCell = LateInitCell<HashSet<BunyipsSubsystem>>()
+    /**
+     * Subsystems managed by the Scheduler.
+     */
     @JvmStatic
     var subsystems by subsystemsCell
     private val scheduledTasks = ArrayList<ScheduledTask>()
     private var taskIdCount = 0
     private var disabled = false
+    private var muted = false
 
     @JvmStatic
     @Hook(on = Hook.Target.PRE_START)
@@ -42,15 +53,15 @@ object Scheduler {
         )
         for (subsystem in subsystems) {
             out.append(" | %\n", subsystem.toVerboseString())
-            for (task in scheduledTasks) {
-                val dep = task.task.dependency
+            for (binding in scheduledTasks) {
+                val dep = binding.task.dependency
                 if (dep.isEmpty || dep.get() != subsystem) continue
-                out.append("    -> %\n", task)
+                out.append("    -> %\n", binding)
             }
         }
-        for (task in scheduledTasks) {
-            if (task.task.dependency.isPresent) continue
-            out.append("  : %\n", task)
+        for (binding in scheduledTasks) {
+            if (binding.task.dependency.isPresent) continue
+            out.append("  : %\n", binding)
         }
         Dbg.logd(out.toString())
         if (subsystems.isEmpty())
@@ -64,55 +75,309 @@ object Scheduler {
         taskIdCount = 0
     }
 
+    /**
+     * Use a specific subset of [subsystems] for the Scheduler.
+     *
+     * By default, all constructed [BunyipsSubsystem] instances will be auto-detected and used.
+     */
     @JvmStatic
     fun use(vararg subsystems: BunyipsSubsystem) {
         this.subsystems = subsystems.toHashSet()
     }
 
+    /**
+     * Schedules a task to execute.
+     *
+     * **Note:** Using this method directly is not recommended,
+     * you should create a binding with [on], [gamepad1], or [gamepad2].
+     *
+     * This method can be manually called for advanced operations such as detaching or forking tasks.
+     */
+    @JvmStatic
+    fun schedule(task: Task) {
+
+    }
+
+    /**
+     * Updates subsystems, runs bindings, and schedules and executes tasks.
+     */
     @JvmStatic
     fun update() {
         if (disabled) return
+
+        if (!muted) {
+            // TODO: test these two counters
+            val tasksWithDependencies = scheduledTasks.stream().filter { it.task.dependency.isPresent }.count()
+            // Task count will account for tasks on subsystems that are not IdleTasks, and also subsystem tasks
+            val taskCount = scheduledTasks.size - tasksWithDependencies + subsystems.size -
+                    subsystems.stream().filter { it.isIdle }.count()
+            DualTelemetry.smartAdd(
+                "\nRunning % task% (%s, %c) on % subsystem%",
+                taskCount,
+                if (taskCount == 1L) "" else "s",
+                tasksWithDependencies + taskCount - scheduledTasks.size,
+                scheduledTasks.size - tasksWithDependencies,
+                subsystems.size,
+                if (subsystems.size == 1) "" else "s"
+            )
+            for (subsystem in subsystems) {
+                val task = subsystem.currentTask
+                if (task == null || task is IdleTask) continue
+                var report = Text.format(
+                    "<small><b>%</b>% <font color='gray'>|</font> <b>%</b> -> %",
+                    subsystem,
+                    if (subsystem.isRunningDefaultTask) " (d.)" else "",
+                    task.toString().replace("$subsystem:", ""),
+                    task.deltaTime to Seconds round 1
+                )
+                val timeoutSec = task.timeout to Seconds
+                report += if (timeoutSec == 0.0) "s" else "/" + timeoutSec + "s"
+                report += "</small>"
+                DualTelemetry.smartAdd(format = report)
+            }
+            for (binding in scheduledTasks) {
+                if (binding.task.dependency.isPresent // Whether the task is never run from the Scheduler (and task reports were handled above)
+                    || !binding.task.isRunning // Whether this task is actually running
+                    || binding.muted // Whether the task has declared itself as muted
+                ) {
+                    continue
+                }
+                val deltaTime = binding.task.deltaTime to Seconds round 1
+                DualTelemetry.smartAdd(
+                    "<small><b>Scheduler</b> (c.) <font color='gray'>|</font> <b>%</b> -> %</small>",
+                    binding.task,
+                    if (deltaTime == 0.0) "active" else deltaTime.toString() + "s"
+                )
+            }
+        }
+
         // TODO
     }
 
+    /**
+     * Disables ability to call [update].
+     */
     @JvmStatic
     fun disable() {
         disabled = true
     }
 
+    /**
+     * Resumes ability to call [update].
+     */
     @JvmStatic
     fun enable() {
         disabled = false
     }
 
+    /**
+     * Unbinds a scheduled task by the [id].
+     *
+     * The [id] is equivalent to the zero-indexed number of constructed binds from initialisation.
+     */
     @JvmStatic
-    fun unbind(index: Int) {
-        // TODO
+    fun unbind(id: Int) {
+        scheduledTasks.removeIf { it.id == id }
     }
 
-    // TODO
-    //  on(BooleanSupplier) to transform into a Trigger
-    //  gamepad1() [gamepad button() and axis controls from previous classes to transform into Trigger]
-    //  gamepad2()
-    //  docs for public things
-
-    class Trigger(val condition: BooleanSupplier) {
-        // TODO
-        //  onChange(Task) [trigger implementation extends Condition]
-        //  onTrue(Task)   [when one of these methods are called, add ScheduledTask based on current state]
-        //  onFalse(Task)
-        //  whileTrue(Task)
-        //  whileFalse(Task)
-        //  toggleOnTrue(Task)
-        //  toggleOnFalse(Task)
-        //  and(Trigger)
-        //  or(Trigger)
-        //  negate()/not()
+    /**
+     * Mutes Scheduler telemetry.
+     */
+    @JvmStatic
+    fun mute() {
+        muted = true
     }
 
-    // (prev, curr) -> run
-    data class ScheduledTask(val task: Task, val condition: BiFunction<Boolean, Boolean, Boolean>) {
-        // TODO: with the trigger system assigning an ID here might be inaccessible
+    /**
+     * Unmutes Scheduler telemetry.
+     */
+    @JvmStatic
+    fun unmute() {
+        muted = false
+    }
+
+    /**
+     * Creates a [Trigger] for the boolean [condition].
+     */
+    infix fun on(condition: BooleanSupplier) = Trigger(condition)
+
+    /**
+     * Creates a button or axis trigger binder to create a [Trigger] for `gamepad1`.
+     */
+    fun gamepad1() = GamepadTrigger(BunyipsLib.opMode.gamepad1)
+
+    /**
+     * Creates a button or axis trigger binder to create a [Trigger] for `gamepad2`.
+     */
+    fun gamepad2() = GamepadTrigger(BunyipsLib.opMode.gamepad2)
+
+    /**
+     * Button or axis binding creator.
+     */
+    class GamepadTrigger(val gamepad: Gamepad) {
+        // TODO
+        //  fun button(button: Controls)
+        //  fun axis(...)
+    }
+
+    /**
+     * A [condition] that will link to and execute some [Task].
+     */
+    class Trigger(val condition: BooleanSupplier): Condition<Trigger>(condition) {
+        private var last: ScheduledTask? = null
+
+        /**
+         * Schedules [task] when the [condition] changes from `false` to `true`, or `true` to `false`.
+         */
+        infix fun onChange(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (prev != curr)
+                    schedule(task)
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `false` to `true`.
+         */
+        infix fun onTrue(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (!prev && curr)
+                    schedule(task)
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `true` to `false`.
+         */
+        infix fun onFalse(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (prev && !curr)
+                    schedule(task)
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `false` to `true`,
+         * and finishes the task when the same condition changes back to `false`.
+         *
+         * Does not restart the task if it finishes by itself while the condition is still `true`.
+         * Use a [RepeatTask] for this behaviour.
+         */
+        infix fun whileTrue(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (!prev && curr)
+                    schedule(task)
+                else if (prev && !curr)
+                    task.finish()
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `true` to `false`,
+         * and finishes the task when the same condition changes back to `true`.
+         *
+         * Does not restart the task if it finishes by itself while the condition is still `false`.
+         * Use a [RepeatTask] for this behaviour.
+         */
+        infix fun whileFalse(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (prev && !curr)
+                    schedule(task)
+                else if (!prev && curr)
+                    task.finish()
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `false` to `true`,
+         * and finishes the task under the same condition (`false` to `true`) if the task is running.
+         */
+        infix fun toggleOnTrue(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (!prev && curr) {
+                    if (task.isRunning) {
+                        task.finish()
+                    } else {
+                        schedule(task)
+                    }
+                }
+            }})
+        }
+
+        /**
+         * Schedules [task] when the [condition] changes from `true` to `false`,
+         * and finishes the task under the same condition (`true` to `false`) if the task is running.
+         */
+        infix fun toggleOnFalse(task: Task) = apply {
+            scheduledTasks.add(ScheduledTask(task) { prev, curr -> {
+                if (prev && !curr) {
+                    if (task.isRunning) {
+                        task.finish()
+                    } else {
+                        schedule(task)
+                    }
+                }
+            }})
+        }
+
+        /**
+         * Descends into the last binded [ScheduledTask] for this trigger.
+         *
+         * Useful for accessing binding-specific information to stop building more [ScheduledTask] instances.
+         */
+        fun descend(): ScheduledTask {
+            return last ?: throw Exceptions.EmergencyStop("Cannot descend into a ScheduledTask as none have been bound for this specific Trigger.")
+        }
+
+        override fun newInstance(edge: Edge, supplier: BooleanSupplier): Trigger {
+            return Trigger(supplier).also {
+                it.edge = edge
+                it.last = last
+            }
+        }
+
+        /**
+         * Alias for [not].
+         */
+        fun negate() = not()
+
+        /**
+         * Alias for [and].
+         */
+        operator fun plus(other: Trigger) = and(other)
+
+        /**
+         * Alias for [or].
+         */
+        operator fun div(other: Trigger) = or(other)
+    }
+
+    /**
+     * A binded [task] that will schedule on the given [binding], accepting the previous state
+     * of the trigger and current state of the trigger.
+     */
+    // (prev, curr)
+    data class ScheduledTask(val task: Task, val binding: BiConsumer<Boolean, Boolean>) {
+        internal var muted = false
+
+        /**
+         * Sequential [id] for this scheduled task that can be used to unbind this task.
+         */
+        @JvmField
         val id = taskIdCount++
+
+        /**
+         * Mute telemetry for this scheduled task.
+         */
+        fun mute() {
+            muted = true
+        }
+
+        /**
+         * Unmute telemetry for this scheduled task.
+         */
+        fun unmute() {
+            muted = false
+        }
     }
 }
--- src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/logic/Condition.java
+++ src/main/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/logic/Condition.java
@@ -12,11 +12,15 @@ import au.edu.sa.mbhs.studentrobotics.bunyipslib.external.units.Time;
 
 /**
  * Collection of {@link BooleanSupplier} extensions for rising and falling edge detection.
+ * <p>
+ * Supports self-typing for type-safe chaining of boolean conditions.
  *
  * @author Lucas Bubner, 2024
  * @since 6.0.0
+ * @param <T> self-type
  */
-public class Condition implements BooleanSupplier {
+@SuppressWarnings("unchecked")
+public class Condition<T extends Condition<T>> implements BooleanSupplier {
     private final BooleanSupplier condition;
     /**
      * The currently set edge detection to use for the {@link BooleanSupplier} {@link #getAsBoolean()} method.
@@ -28,18 +32,26 @@ public class Condition implements BooleanSupplier {
     private boolean lastState;
 
     /**
-     * Creates a new Condition with the given {@link BooleanSupplier}.
+     * Creates a new self-type Condition with the given {@link BooleanSupplier}.
      *
      * @param edge      The edge detection to use.
      * @param condition The {@link BooleanSupplier} to use.
      */
     public Condition(@NonNull Edge edge, @NonNull BooleanSupplier condition) {
-        this.condition = condition;
         this.edge = edge;
+        if (condition instanceof Condition<?> self) {
+            // Flatten new Condition instances
+            this.condition = self.condition;
+            self.delayNs = delayNs;
+            self.capture = capture;
+            self.lastState = lastState;
+        } else {
+            this.condition = condition;
+        }
     }
 
     /**
-     * Creates a new Condition with the given {@link BooleanSupplier} and no edge detection.
+     * Creates a new self-type Condition with the given {@link BooleanSupplier} and no edge detection.
      *
      * @param condition The {@link BooleanSupplier} to use.
      */
@@ -48,7 +60,20 @@ public class Condition implements BooleanSupplier {
     }
 
     /**
-     * Composes this Condition with a delay. The delay will be applied to the output of all exposed methods of this
+     * Mutates this Condition to adjust the edge detection for
+     * the {@link BooleanSupplier} {@link #getAsBoolean()} method.
+     *
+     * @param edge the edge to use
+     * @return self with the edge applied
+     */
+    @NonNull
+    public T withEdge(@NonNull Edge edge) {
+        this.edge = edge;
+        return (T) this;
+    }
+
+    /**
+     * Mutates this Condition with a delay. The delay will be applied to the output of all exposed methods of this
      * class and dictates a minimum time that the original condition must be true for the output to be true.
      * <p>
      * This is useful for ensuring that a condition is true for a certain amount of time before acting on it.
@@ -58,17 +83,17 @@ public class Condition implements BooleanSupplier {
      * will fire after the delay has passed after the condition has transitioned from true to false and has remained false.
      *
      * @param delay the delay to apply to the condition. Zero or negative values will disable the delay (default).
-     * @return this Condition with the delay applied
+     * @return self with the delay applied
      */
     @NonNull
-    public Condition withActiveDelay(@Nullable Measure<Time> delay) {
+    public T withActiveDelay(@Nullable Measure<Time> delay) {
         if (delay == null) {
             delayNs = 0;
-            return this;
+            return (T) this;
         }
         double d = delay.in(Nanoseconds);
         delayNs = d <= 0 ? 0 : (long) d;
-        return this;
+        return (T) this;
     }
 
     /**
@@ -143,46 +168,57 @@ public class Condition implements BooleanSupplier {
     }
 
     /**
-     * Creates a new Condition that is the logical OR of this Condition and another {@link BooleanSupplier}.
+     * Override this method and construct a new instance of the subclass for self-typing.
+     *
+     * @param edge supplied edge for construction
+     * @param supplier supplied supplier for construction
+     * @return new instance of type T
+     */
+    protected T newInstance(Condition.Edge edge, BooleanSupplier supplier) {
+        return (T) new Condition<>(edge, supplier);
+    }
+
+    /**
+     * Creates a new self-type Condition that is the logical OR of this Condition and another {@link BooleanSupplier}.
      *
      * @param other The other {@link BooleanSupplier} to OR with.
-     * @return A new Condition that is the logical OR of this Condition and the other {@link BooleanSupplier}.
+     * @return A new self-type Condition that is the logical OR of this Condition and the other {@link BooleanSupplier}.
      */
     @NonNull
-    public Condition or(@NonNull BooleanSupplier other) {
-        return new Condition(edge, () -> getAsBoolean() || other.getAsBoolean());
+    public T or(@NonNull BooleanSupplier other) {
+        return newInstance(edge, () -> getAsBoolean() || other.getAsBoolean());
     }
 
     /**
-     * Creates a new Condition that is the logical AND of this Condition and another {@link BooleanSupplier}.
+     * Creates a new self-type Condition that is the logical AND of this Condition and another {@link BooleanSupplier}.
      *
      * @param other The other {@link BooleanSupplier} to AND with.
-     * @return A new Condition that is the logical AND of this Condition and the other {@link BooleanSupplier}.
+     * @return A new self-type Condition that is the logical AND of this Condition and the other {@link BooleanSupplier}.
      */
     @NonNull
-    public Condition and(@NonNull BooleanSupplier other) {
-        return new Condition(edge, () -> getAsBoolean() && other.getAsBoolean());
+    public T and(@NonNull BooleanSupplier other) {
+        return newInstance(edge, () -> getAsBoolean() && other.getAsBoolean());
     }
 
     /**
-     * Creates a new Condition that is the logical XOR of this Condition and another {@link BooleanSupplier}.
+     * Creates a new self-type Condition that is the logical XOR of this Condition and another {@link BooleanSupplier}.
      *
      * @param other The other {@link BooleanSupplier} to XOR with.
-     * @return A new Condition that is the logical XOR of this Condition and the other {@link BooleanSupplier}.
+     * @return A new self-type Condition that is the logical XOR of this Condition and the other {@link BooleanSupplier}.
      */
     @NonNull
-    public Condition xor(@NonNull BooleanSupplier other) {
-        return new Condition(edge, () -> getAsBoolean() ^ other.getAsBoolean());
+    public T xor(@NonNull BooleanSupplier other) {
+        return newInstance(edge, () -> getAsBoolean() ^ other.getAsBoolean());
     }
 
     /**
-     * Creates a new Condition that is the logical NOT of this Condition.
+     * Creates a new self-type Condition that is the logical NOT of this Condition.
      *
-     * @return A new Condition that is the logical NOT of this Condition.
+     * @return A new self-type Condition that is the logical NOT of this Condition.
      */
     @NonNull
-    public Condition not() {
-        return new Condition(edge, () -> !getAsBoolean());
+    public T not() {
+        return newInstance(edge, () -> !getAsBoolean());
     }
 
     @NonNull
