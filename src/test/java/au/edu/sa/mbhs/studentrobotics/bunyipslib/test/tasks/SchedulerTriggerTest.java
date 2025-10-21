d2eeff18beeb39ddf1795e7a1c4ea5c710f1fd48
Lucas Bubner <lkbubner@proton.me>
2025-10-22 10:12:33 +1030

    Integrate more unit tests

--- src/test/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/test/tasks/SchedulerTriggerTest.java
+++ src/test/java/au/edu/sa/mbhs/studentrobotics/bunyipslib/test/tasks/SchedulerTriggerTest.java
@@ -30,28 +30,75 @@ class SchedulerTriggerTest {
         Dbg.INSTANCE.setINHIBIT(true);
         Dashboard.INHIBIT_PACKETS = true;
         DualTelemetry.Companion.setINHIBIT_SMART_CALLS(true);
-    }
-
-    @Test
-    void onTrueTest() {
-        AtomicBoolean finished = new AtomicBoolean(false);
-        Task task = Task.task().isFinished(finished::get);
-
-        Gamepad gamepad = new Gamepad();
-        gamepad.a = false;
-        // Can't access implicit methods
-        new Scheduler.GamepadTrigger(GamepadUser.ONE, gamepad).button(Controls.A)
-                .onTrue(task);
         // Required to register
         new BunyipsSubsystem() {
             @Override
             protected void periodic() {
             }
         };
+    }
+
+    @Test
+    void onTrueTest() {
+        AtomicBoolean finished = new AtomicBoolean(false);
+        Task task = Task.task().isFinished(finished::get);
+        Gamepad gamepad = new Gamepad();
+
+        gamepad.a = false;
+        // Can't access implicit methods
+        new Scheduler.GamepadTrigger(GamepadUser.ONE, gamepad).button(Controls.A)
+                .onTrue(task);
         Scheduler.update();
         assertFalse(task.isRunning());
         gamepad.a = true;
         Scheduler.update();
+        assertTrue(task.isRunning());
+        finished.set(true);
+        Scheduler.update();
+        assertFalse(task.isRunning());
+    }
+
+    @Test
+    void onFalseTest() {
+        AtomicBoolean finished = new AtomicBoolean(false);
+        Task task = Task.task().isFinished(finished::get);
+        Gamepad gamepad = new Gamepad();
+
+        gamepad.b = true;
+        new Scheduler.GamepadTrigger(GamepadUser.ONE, gamepad).button(Controls.B)
+                .onFalse(task);
+        Scheduler.update();
+        assertFalse(task.isRunning());
+        gamepad.b = false;
+        Scheduler.update();
+        assertTrue(task.isRunning());
+        finished.set(true);
+        Scheduler.update();
+        assertFalse(task.isRunning());
+    }
+
+    @Test
+    void onChangeTest() {
+        AtomicBoolean finished = new AtomicBoolean(false);
+        Task task = Task.task().isFinished(finished::get).named("taska");
+        Gamepad gamepad = new Gamepad();
+
+        gamepad.x = true;
+        new Scheduler.GamepadTrigger(GamepadUser.ONE, gamepad).button(Controls.X)
+                .onChange(task);
+        Scheduler.update();
+        assertFalse(task.isRunning());
+        gamepad.x = false;
+        Scheduler.update();
+        assertTrue(task.isRunning());
+        finished.set(true);
+        Scheduler.update(); // task finish condition stops running the task
+        assertFalse(task.isRunning());
+        Scheduler.update(); // task onFinish fires and is removed
+        assertFalse(Scheduler.activeTasks.contains(task));
+        finished.set(false);
+        gamepad.x = true;
+        Scheduler.update(); // rescheduled
         assertTrue(task.isRunning());
         finished.set(true);
         Scheduler.update();
